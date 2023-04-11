ca2b12fc18d2ebe61d0abb7b77b1028cb58adc5f
Abdullah Saim <94314354+ASa1m@users.noreply.github.com>
2023-04-12 03:12:01 +0500

    Update Time.py

--- Time.py
+++ Time.py
@@ -3,6 +3,8 @@ import datetime
 from datetime import datetime, date, time, timedelta
 
 header = ["Monday","Tuesday","Wednesday","Thursday","Friday"]
+dep = "BSCS"
+sec = ["A","B","C"]
 
 Time = []
 timeobj= time(9,00,00)
@@ -16,18 +18,33 @@ def emptytimetable():
 Timetable = [[[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]]
 
 def gen(timetable):
-    subs = [["Com",0,2],["Dis",0,3],["ICT",0,2],["ICT Lab",0,3],["FOCP",0,3],["FOCP Lab",0,3],["PKST",0,2],["Cal",0,3],["-",0,100]]
-    for x in range(7):
-        for y in range(5):
-            while True:
-                s = random.randint(0,8)
-                if subs[s][1] < subs[s][2]:
-                    timetable[x][y] = subs[s][0]
-                    subs[s][1] += 1
-                    break
-                
-    timetable.insert(4,["-","-","-","-","-"])
-    return timetable
+    
+    while True:
+        subs = [["Com",0,2],["Dis",0,3],["ICT",0,2],["ICT Lab",0,3],["FOCP",0,3],["FOCP Lab",0,3],["PKST",0,2],["Cal",0,3],["-",0,20]]
+        for x in range(6,-1,-1):
+            for y in range(5):
+                while True:
+                    s = random.randint(0,len(subs)-1)
+                    if timetable[x][y] != 0: break
+                    if "Lab" in subs[s][0] and subs[s][1] < subs[s][2]:
+                        if x==1 or x==4:
+                            timetable[x][y] = timetable[x+1][y] = timetable[x+2][y] = subs[s][0]
+                            subs[s][1] = 3
+                            break
+                    elif "Lab" not in subs[s][0] and subs[s][1] < subs[s][2]:
+                        timetable[x][y] = subs[s][0]
+                        subs[s][1] += 1
+                        break
+
+                    
+                    
+        timetable.insert(4,["-","-","-","-","-"])
+        sum = 0
+        for i in range(len(subs)-1):
+            sum += subs[i][1]
+        if sum == 21:
+            return timetable
+        
 
 
 def conflict(table,temp):
@@ -41,21 +58,26 @@ def Table_add():
     global Timetable
     while True:
         temp = gen(emptytimetable())
+
+        
         for table in Timetable:
             if conflict(table,temp):
                 Timetable.append(temp)
                 return    
 
-        #temp.insert(4,["-","-","-","-","-"])
+Timetable[0] = gen(emptytimetable())
 
     
-Timetable[0] = gen(emptytimetable())
 for x in range(2):
+    
     Table_add()
 
 
-
+itr_sec = 0
 for timetables in Timetable:
+    print()
+    print ("\t\t\t\t",dep,"Timetable of Section",sec[itr_sec])
+    print("-"*100)
     print("Dates".center(20),end='|')
     for x in header:
         print(x.center(15),end='|')
@@ -69,3 +91,4 @@ for timetables in Timetable:
             print(y.center(15),end='|')
         print()
     print()
+    itr_sec += 1
